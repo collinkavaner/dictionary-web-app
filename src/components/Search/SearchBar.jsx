4798f3a6db0617505ca846e3b9152cd976143f2a
Collin <collinkavaner@gmail.com>
2023-08-20 17:35:01 -0400

    fixed search icon

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Search from '../../assets/search.svg';
 
 const SearchBar = () => {
     return (
@@ -17,13 +18,13 @@ const SearchBarWrapper = styled.form`
 const SearchBarInput = styled.input`
     width: 100%;
     height: 64px;
-    padding: 24px;
+    padding: 24px 64px 24px 24px;
     border-radius: 16px;
     border: none;
     background-color: #F4F4F4;
     font-size: 1.25rem; // 20px
     font-weight: 700; 
-    background-image: url('./src/assets/search.svg');
+    background-image: url(${Search});
     background-repeat: no-repeat;
     background-position: 95% 50%;
 
